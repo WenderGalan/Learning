9f6acb1ba3e8a567dd71282943aab33c732a219d
Wender Galan <wendergalan2014@hotmail.com>
2018-01-04 17:28:48 -0300

    Projeto em branco do Parse versão 1.13.0

---
+++ Android Studio/Parse-Projeto-1.13.0/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Habilite armazenamento local.
+    Parse.enableLocalDatastore(this);
+
+    // Codigo de configuração do App
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("APPLICATION_ID_AQUI")
+            .clientKey(null)
+            .server("URL_SERVIDOR")
+    .build()
+    );
+
+    /*
+      // Teste de configuração do App
+      ParseObject pontuacao = new ParseObject("Pontuacao");
+      pontuacao.put("pontos", 100);
+      pontuacao.saveInBackground(new SaveCallback() {
+          public void done(ParseException e) {
+              if (e == null) {
+                  Log.i("TesteExecucao", "Salvo com sucesso!!!");
+              } else {
+                  Log.i("TesteExecucao", "Falha ao salvar os dados!!!");
+              }
+          }
+      });
+    */
+
+      ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    // Optionally enable public read access.
+    // defaultACL.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
