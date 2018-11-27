089f2276835cbbca2e4eb9bc646ea29e52ce58d7
Wender Galan <wendergalan2014@hotmail.com>
2018-11-27 08:38:07 -0300

    Alterações nos projetos...

---
+++ Kotlin/MyApplicationWithKotlinMixing/app/src/main/java/com/wendergalan/myapplicationwithkotlinmixing/Animal.kt
@@ -0,0 +1,7 @@
+package com.wendergalan.myapplicationwithkotlinmixing
+
+abstract class Animal{
+
+    abstract fun fazBarulho(): String
+    abstract fun isVivo(): Boolean
+}
---
+++ Kotlin/MyApplicationWithKotlinMixing/app/src/main/java/com/wendergalan/myapplicationwithkotlinmixing/Cachorro.kt
@@ -0,0 +1,12 @@
+package com.wendergalan.myapplicationwithkotlinmixing
+
+class Cachorro : Animal() {
+    override fun fazBarulho(): String {
+        TODO("not implemented")
+    }
+
+    override fun isVivo(): Boolean {
+        TODO("not implemented")
+    }
+
+}
